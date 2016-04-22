a5012e12aa573aed537f0ba46e473f987acdf9fd
Sergey Shandar <sergey.shandar@gmail.com>
2016-04-22 00:01:59 -0700

    GroupBy changes.

--- PureLogic/PureLogic/GroupBy.cs
+++ PureLogic/PureLogic/GroupBy.cs
@@ -4,6 +4,16 @@ namespace PureLogic
 {
     public sealed class GroupBy<K, T> : Reduce<Tuple<K, T>>
     {
+        public Bag<Tuple<K, T>> Input { get; }
+
+        public Func<T, T, T> Func { get; }
+
         public override R Accept<R>(IVisitor<R> visitor) => visitor.Visit(this);
+
+        public GroupBy(Bag<Tuple<K, T>> input, Func<T, T, T> func)
+        {
+            Input = input;
+            Func = func;
+        }
     }
 }
