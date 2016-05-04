eb73efc81083035fb261de8d101f418013fda75f
Sergey Shandar <sergey.shandar@gmail.com>
2016-05-03 22:57:34 -0700

    iteration

--- PureLogic/PureLogic/One.cs
+++ PureLogic/PureLogic/One.cs
@@ -1,12 +1,8 @@
 ﻿namespace PureLogic
 {
-    public sealed class One: Reduce<Void>
+    public abstract class One: Reduce<Void>
     {
         private One() { }
-
-        public static readonly One Value = new One();
-
-        public override R Accept<R>(IVisitor<R> visitor) => visitor.Visit(this);
     }
 
     public static class OneX
