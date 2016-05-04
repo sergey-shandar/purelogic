f5d4eb8bad3b04ec125a9b432a18369c2bdb8c38
Sergey Shandar <sergey.shandar@gmail.com>
2016-05-04 07:17:22 -0700

    iteration

--- PureLogic/PureLogic/SelectMany.cs
+++ PureLogic/PureLogic/SelectMany.cs
@@ -1,22 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace PureLogic
 {
-    public abstract class SelectMany<I, T> : Bag<T>
-    {
-    }
-
     public static class SelectManyX
     {
-        public static Bag<T> SelectMany<I, T>(this Bag<I> input, Func<I, IEnumerable<T>> func)
+        public static Bag<T> Select<I, T>(this Bag<I> input, Func<I, T> func)
             => null;
 
-        public static Bag<T> Select<I, T>(this Bag<I> input, Func<I, T> func)
-            => input.SelectMany(i => new[] { func(i) });
-
         public static Bag<T> Where<T>(this Bag<T> input, Func<T, bool> func)
-            => input.SelectMany(i => func(i) ? Enumerable.Empty<T>() : new[] { i });
+            => null;
     }
 }
