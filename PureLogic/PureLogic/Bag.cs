674e6f5ad17bd7748e86e29a9e3b9873a8e26f6a
Sergey Shandar <sergey.shandar@gmail.com>
2016-05-04 07:47:49 -0700

    one file

--- PureLogic/PureLogic/Bag.cs
+++ PureLogic/PureLogic/Bag.cs
@@ -1,6 +1,20 @@
-﻿namespace PureLogic
+﻿using System;
+
+namespace PureLogic
 {
-    public abstract class Bag
+    public class Bag
     {
+    }
+
+    public static class SelectManyX
+    {
+        public static Bag Select(this Bag input, Func<int, int> func)
+            => null;
+
+        public static Bag Where(this Bag input, Func<int, bool> func)
+            => null;
+
+        public static Bag A()
+            => from r in Bag where true select 7;
     }
 }
--- PureLogic/PureLogic/SelectMany.cs
+++ PureLogic/PureLogic/SelectMany.cs
@@ -2,12 +2,5 @@
 
 namespace PureLogic
 {
-    public static class SelectManyX
-    {
-        public static Bag Select(this Bag input, Func<int, int> func)
-            => null;
 
-        public static Bag Where(this Bag input, Func<int, bool> func)
-            => null;
-    }
 }
