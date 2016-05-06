a789b4e78744b97c17913d472d4326bd2817a4d3
Sergey Shandar <sergey.shandar@gmail.com>
2016-05-06 09:58:59 -0700

    Aggregate

--- PureLogic/PureLogic/AggregateX.cs
+++ PureLogic/PureLogic/AggregateX.cs
@@ -15,5 +15,67 @@ namespace PureLogic
             => input
                 .Select(OptionX.Option)
                 .Aggregate(new Option<T>(), func.OptionBinaryFunc);
+
+        public static Bag<bool> AllTrue(this Bag<bool> input)
+            => input.Aggregate(true, (a, b) => a && b);
+
+        public static Bag<bool> All<T>(this Bag<T> input, Func<T, bool> f)
+            => input.Select(f).AllTrue();
+
+        public static Bag<bool> AnyTrue(this Bag<bool> input)
+            => input.Aggregate(false, (a, b) => a || b);
+
+        public static Bag<bool> Any<T>(this Bag<T> input, Func<T, bool> f)
+            => input.Select(f).AnyTrue();
+
+        public static Bag<bool> Any<T>(this Bag<T> input)
+            => input.Any(_ => true);
+
+        public static Bag<bool> Contains<T>(this Bag<T> input, T value)
+            => input.Any(v => v.Equals(value));
+
+        public static Bag<T> DefaultIfEmpty<T>(this Bag<T> input, T value = default(T))
+            => input
+                .Any()
+                .Where(any => !any)
+                .Select(_ => value)
+                .DisjointUnion(input);
+
+        public static Bag<Option<T>> Min<T>(this Bag<T> input)
+            where T : IComparable
+            => input.Aggregate((a, b) => a.CompareTo(b) <= 0 ? a : b);
+
+        public static Bag<Option<T>> Max<T>(this Bag<T> input)
+            where T : IComparable
+            => input.Aggregate((a, b) => a.CompareTo(b) >= 0 ? a : b);
+
+        public static Bag<Option<T>> Average<P, T>(this P policy, Bag<T> input)
+            where P : struct, INumericPolicy<T>
+            => input
+                .Select(v => Tuple.Create(v, 1L))
+                .Aggregate((a, b) => Tuple.Create(policy.Plus(a.Item1, b.Item1), a.Item2 + b.Item2))
+                .Select(x => x.Select(v => policy.Div(v.Item1, policy.FromLong(v.Item2))));
+
+        public static Bag<Option<double>> Average(this Bag<double> input)
+            => new NumericPolicy().Average(input);
+
+        public static Bag<Option<decimal>> Average(this Bag<decimal> input)
+            => new NumericPolicy().Average(input);
+
+        public static Bag<long> Count<T>(this Bag<T> input)
+            => input.Select(_ => 1L).Sum();
+
+        public static Bag<T> Sum<P, T>(this P policy, Bag<T> input)
+            where P : struct, INumericPolicy<T>
+            => input.Aggregate(policy.FromLong(0), policy.Plus);
+
+        public static Bag<long> Sum(this Bag<long> input)
+            => new NumericPolicy().Sum(input);
+
+        public static Bag<double> Sum(this Bag<double> input)
+            => new NumericPolicy().Sum(input);
+
+        public static Bag<decimal> Sum(this Bag<decimal> input)
+            => new NumericPolicy().Sum(input);
     }
 }
--- PureLogic/PureLogic/GroupBy.cs
+++ PureLogic/PureLogic/GroupBy.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace PureLogic
 {
@@ -28,39 +29,6 @@ namespace PureLogic
             this Bag<T> input, Func<T, K> key, Func<T, T, T> reduce)
             => input.Select(v => key(v).WithValue(v)).GroupBy(reduce);
 
-        public static Bag<bool> All(this Bag<bool> input)
-            => input.Aggregate(true, (a, b) => a && b);
-
-        public static Bag<bool> Any(this Bag<bool> input)
-            => input.Aggregate(false, (a, b) => a || b);
-
-        public static Bag<Option<T>> Average<P, T>(this P policy, Bag<T> input)
-            where P : struct, INumericPolicy<T>
-            => input
-                .Select(v => Tuple.Create(v, 1L))
-                .Aggregate((a, b) => Tuple.Create(policy.Plus(a.Item1, b.Item1), a.Item2 + b.Item2))
-                .Select(x => x.Select(v => policy.Div(v.Item1, policy.FromLong(v.Item2))));
-
-        public static Bag<Option<double>> Average(this Bag<double> input)
-            => new NumericPolicy().Average(input);
-
-        public static Bag<Option<decimal>> Average(this Bag<decimal> input)
-            => new NumericPolicy().Average(input);
-
-        public static Bag<bool> Contains<T>(this Bag<T> input, T value)
-            => input.Where(v => v.Equals(value)).Select(_ => true);
-
-        public static Bag<long> Count<T>(this Bag<T> input)
-            => input.Select(_ => 1L).Sum();
-
-        public static Bag<T> DefaultIfEmpty<T>(this Bag<T> input, T value = default(T))
-            => input
-                .Select(_ => true)
-                .Any()
-                .Where(any => !any)
-                .Select(_ => value)
-                .DisjointUnion(input);
-
         public static Bag<T> Distinct<T>(this Bag<T> input)
             => input
                 .Select(v => v.WithValue(new Void()))
@@ -77,32 +45,20 @@ namespace PureLogic
                 .DisjointUnion(b.ToMultiSet(0L, 1L))
                 .GroupBy((x, y) => Tuple.Create(x.Item1 + y.Item1, x.Item2 + y.Item2));
 
-        private static Bag<T> DifWhere<T>(
-            this Bag<T> a, Bag<T> b, Func<Tuple<long, long>, bool> func)
-            => a.Dif(b)
-                .Where(v => func(v.Value))
-                .Select(v => v.Key);
+        private static Bag<T> DifSelectMany<T>(
+            this Bag<T> a, Bag<T> b, Func<Tuple<long, long>, long> func)
+            => a.Dif(b).SelectMany(v => Enumerable.Repeat(v.Key, (int)func(v.Value)));
 
         public static Bag<T> Except<T>(this Bag<T> a, Bag<T> b)
-            => a.DifWhere(b, v => v.Item1 == 0 || v.Item2 == 0);
+            => a.DifSelectMany(b, v => v.Item1 - v.Item2);
 
         public static Bag<T> Intersect<T>(this Bag<T> a, Bag<T> b)
-            => a.DifWhere(b, v => v.Item1 > 0 && v.Item2 > 0);
+            => a.DifSelectMany(b, v => Math.Min(v.Item1, v.Item2));
 
         public static Bag<T> Union<T>(this Bag<T> a, Bag<T> b)
-            => a.Dif(b).Select(v => v.Key);
+            => a.DifSelectMany(b, v => Math.Max(v.Item1, v.Item2));
 
-        public static Bag<T> Sum<P, T>(this P policy, Bag<T> input)
-            where P : struct, INumericPolicy<T>
-            => input.Aggregate(policy.FromLong(0), policy.Plus);
-
-        public static Bag<long> Sum(this Bag<long> input)
-            => new NumericPolicy().Sum(input);
-
-        public static Bag<double> Sum(this Bag<double> input)
-            => new NumericPolicy().Sum(input);
-
-        public static Bag<decimal> Sum(this Bag<decimal> input)
-            => new NumericPolicy().Sum(input);
+        public static Bag<bool> BagEqual<T>(this Bag<T> a, Bag<T> b)
+            => a.Dif(b).All(v => v.Value.Item1 == v.Value.Item2);
     }
 }
